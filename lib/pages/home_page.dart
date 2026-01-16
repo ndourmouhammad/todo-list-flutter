673f62d1a0f4129d2001bafbcc4750d88bb729e3
ndourmouhammad <ndourmouhammad15@gmail.com>
2026-01-16 13:15:28 +0000

    feat: Build UI with Consumer and add CRUD operations

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,8 @@ import 'package:todo/providers/task_provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
